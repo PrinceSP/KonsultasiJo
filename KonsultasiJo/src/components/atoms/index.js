91c8c5ce8b8e266302fd6759a8042fda91c6959e
PrinceSP <princepasombaran@gmail.com>
2022-10-11 08:16:15 +0800

    change text input component

--- KonsultasiJo/src/components/atoms/index.js
+++ KonsultasiJo/src/components/atoms/index.js
@@ -1,8 +1,8 @@
-import TextInput from './TextInput'
+import Input from './TextInput'
 import Button from './Button'
 import Gap from './Gap';
 import Berita from './Berita';
 import AKategori from './AKategori';
 
 
-export {TextInput,Button,Gap,Berita,AKategori};
+export {Input,Button,Gap,Berita,AKategori};
