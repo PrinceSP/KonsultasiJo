82ed9db087da894ab48c2e437c87a0012de3a0b2
PrinceSP <princepasombaran@gmail.com>
2022-10-12 00:58:10 +0800

    change color and add static data

--- KonsultasiJo/src/pages/ChatClient/index.js
+++ KonsultasiJo/src/pages/ChatClient/index.js
@@ -1,80 +1,142 @@
-import { StyleSheet, ScrollView, View, Text, TextInput, TouchableOpacity } from 'react-native'
+import { StyleSheet, ScrollView, View, Text, TextInput, TouchableOpacity, ImageBackground, FlatList } from 'react-native'
 import React from 'react'
-import { Header, Gap, ReplyClient } from '../../components';
-import ReplyOperator from '../../components/molecules/ReplyOperator';
+import { Header, Gap, ReplyOperator } from '../../components';
 import { Send } from '../../assets';
 
 const ChatClient = ({navigation}) => {
+  const Data = [
+            {
+                message: 'Yes Ofcourse..',
+                type: 'sender'
+            },
+            {
+                message: 'How are You ?',
+                type: 'sender'
+            },
+            {
+                message: 'How Your Opinion about the one done app ?',
+                type: 'sender'
+            },
+            {
+                message: 'Well i am not satisfied with this design plzz make design better ',
+                type: 'receiver'
+            },
+            {
+                message: 'could you plz change the design...',
+                type: 'receiver'
+            },
+            {
+                message: 'How are You ?',
+                type: 'sender'
+            },
+            {
+                message: 'How Your Opinion about the one done app ?',
+                type: 'sender'
+            },
+            {
+                message: 'Well i am not satisfied with this design plzz make design better ',
+                type: 'receiver'
+            },
+            {
+                message: 'could you plz change the design...',
+                type: 'receiver'
+            },
+            {
+                message: 'How are You ?',
+                type: 'sender'
+            },
+            {
+                message: 'How Your Opinion about the one done app ?',
+                type: 'sender'
+            }
+]
   return (
     <View style={styles.container}>
-        <Header title="Konsultasi" onBack={() => navigation.goBack()} />        
-        <ScrollView showsVerticalScrollIndicator={false}>                        
-        
-                <ReplyOperator marginright={100} />     
-                <Gap height={30} />                   
-        
-                <ReplyClient />
-                <Gap height={30} />            
-        
-                <ReplyOperator />     
-                <Gap height={30} />            
-        
-                <ReplyClient />
-                <Gap height={10} />   
-                
-        </ScrollView>
-        <View style={styles.TypingText}>  
-            <View style={styles.BoxText}>             
-            <ScrollView style={{flex: 1}} showsVerticalScrollIndicator={false}>
-                <View style={styles.TextBox}>
-                    <TextInput placeholder={'Ketik pesan disini'}/>                    
-                </View>                
-            </ScrollView>                           
-                <TouchableOpacity>
-                <View style={styles.SendIcon}>
-                <Send height={30} width={30}/>
-                </View>               
-                </TouchableOpacity>
-            </View>          
-            
-            
-            
-        </View>
+        <ImageBackground
+          source = {require('../../assets/images/backgroundChat.jpeg')}
+          style={{flex: 1}}>
+          <Header title="Konsultasi" onBack={() => navigation.goBack()} />
+          <FlatList style={{ flex: 1}}
+          data={Data}
+          showsVerticalScrollIndicator={false}
+          keyExtractor={(item, index) => index}
+          inverted
+          renderItem={({ item }) => {
+              return (
+                  <ReplyOperator item={item} sender={item.type=="sender"} message={item.message}/>
+              )
+          }}/>
+          <Gap height={30} />
+        </ImageBackground>
+        <View
+          style={{
+              backgroundColor: "#D9D9D9",
+              elevation: 5,
+              // height: 60,
+              flexDirection:'row',
+              alignItems:'center',
+              paddingVertical:7,
+              justifyContent:'space-evenly'
+          }}>
+
+          <TextInput
+              style={{
+                  backgroundColor: "#fff",
+                  width:'80%',
+                  borderRadius:10,
+                  borderWidth:0.5,
+                  borderColor: "#fff",
+                  paddingHorizontal: 15,
+                  color: "#000",
+              }}
+              placeholder = "type a message"
+              placeholderTextColor = "#8C8888"
+              multiline = {true}
+              // value={msg}
+              // onChangeText={(val)=>setMsg(val)}
+          />
+
+          <TouchableOpacity>
+            <View style={styles.SendIcon}>
+            <Send height={30} width={30}/>
+            </View>
+          </TouchableOpacity>
+
+      </View>
     </View>
   )
-} 
- 
+}
+
 export default ChatClient
 
 const styles = StyleSheet.create({
     container:{
-        backgroundColor: 'white',
-        flex: 1,                
+        backgroundColor: '#fff',
+        flex: 1,
     },
     text:{
-        fontSize: 50,        
+        fontSize: 50,
     },
-    TypingText: {                  
-        padding: 10,    
-        backgroundColor: '#D9D9D9',                
+    TypingText: {
+        padding: 10,
+        backgroundColor: '#D9D9D9',
     },
-    BoxText: {                
+    BoxText: {
         flexDirection: 'row',
     },
-    SendIcon: {                    
+    SendIcon: {
         alignSelf: 'flex-end',
         position: 'relative',
         padding: 5,
         borderRadius: 40,
-        backgroundColor: '#696868',        
+        backgroundColor: '#696868',
     },
-    TextBox: {                
-        fontSize: 14,
-        marginRight: 10,
-        marginLeft: 10,
-        backgroundColor: 'white',
-        padding: 1,
-        borderRadius: 10,
-        color: '#8C8888',
+    TextBox: {
+      elevation: 5,
+      // height: 60,
+      flexDirection:'row',
+      alignItems:'center',
+      paddingVertical:7,
+      justifyContent:'space-evenly'
     },
 });
