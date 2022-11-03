b044a55aac000460e81db4d3abbb54e2cc5f4b4f
PrinceSP <princepasombaran@gmail.com>
2022-11-03 13:25:38 +0800

    add forgot password route

--- KonsultasiJo/src/navigations/authStackCustomer.js
+++ KonsultasiJo/src/navigations/authStackCustomer.js
@@ -1,6 +1,6 @@
 import React,{useState,useEffect} from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { Phukum,Statistik,Kategori,SplashScreen, SignIn,SignInOperator, SignUp, Profile, AboutUs, Maps, Menu} from '../pages';
+import { SignIn,SignInOperator, SignUp, ForgotPassword} from '../pages';
 
 const Stack = createNativeStackNavigator();
 
@@ -10,6 +10,7 @@ const AuthStack = () => {
     <Stack.Navigator >
       <Stack.Screen name="SignIn" component={SignIn} options={{headerShown: false}}/>
       <Stack.Screen name="SignUp" component={SignUp} options={{headerShown: false}}/>
+      <Stack.Screen name="ForgotPassword" component={ForgotPassword} options={{headerShown: false}}/>
       <Stack.Screen name="SignInOperator" component={SignInOperator}options={{headerShown: false}}/>
     </Stack.Navigator>
   )
